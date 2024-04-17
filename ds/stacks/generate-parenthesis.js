4e32db44312748a25f067a92c6cb3a480e68d32f
bikraman <bikramankarmakar10@gmail.com>
2024-04-17 18:09:14 +0530

    event loop quiz 4

--- ds/stacks/generate-parenthesis.js
+++ ds/stacks/generate-parenthesis.js
@@ -1,31 +1,4 @@
 
-var isValid = function(s) {
-    
-    const stack = []
-
-    const chars = s.split("");
-
-    const len = chars.length;
-    for (let i = 0; i < len; i++) {
-        if (i === 0) 
-            stack.push(chars[i])
-        else if (stack[stack.length - 1] === "(" && chars[i] === ")") 
-            stack.pop()
-        else if (stack[stack.length - 1] === "{" && chars[i] === "}") 
-            stack.pop()
-        else if (stack[stack.length - 1] === "[" && chars[i] === "]") 
-            stack.pop()
-        else
-            stack.push(chars[i])
-    }
-
-    if (stack.length === 0)
-        return true;
-    else 
-        return false;
-
-};
-
 /**
  * @param {number} n
  * @return {string[]}
@@ -40,30 +13,7 @@ var generateParenthesis = function(n) {
         core.push(")")
     }
 
-    console.log(perm(core).join("\n"));
-
 };
-
-function perm(xs) {
-    let ret = [];
-  
-    for (let i = 0; i < xs.length; i = i + 1) {
-      let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
-  
-      if(!rest.length) {
-        ret.push([xs[i]])
-      } else {
-        for(let j = 0; j < rest.length; j = j + 1) {
-          ret.push([xs[i]].concat(rest[j]))
-        }
-      }
-    }
-
-    return ret;
-}
-  
-
-
 
 generateParenthesis(8);
 
