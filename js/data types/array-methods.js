ed73ec749295f7a48e4cad8c8a03f9389765a985
bikraman <bikramankarmakar10@gmail.com>
2024-05-02 10:11:17 +0530

    map to objects

--- js/data types/array-methods.js
+++ js/data types/array-methods.js
@@ -92,3 +92,25 @@
 // let names = users.map((value) => value.name)
 
 // console.log( names ); // John, Pete, Mary
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = users.map((value) =>  {
+//     return { fullName: `${value.name} ${value.surname}`, id: value.id }
+// })
+
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// console.log( usersMapped[0].id ) // 1
+// console.log( usersMapped[0].fullName ) // John Smith
