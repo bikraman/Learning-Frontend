15481c91fa3c16ca45804259418e6b414943b653
bikraman <bikramankarmakar10@gmail.com>
2024-05-02 07:54:58 +0530

    filtered range

--- js/data types/array-methods.js
+++ js/data types/array-methods.js
@@ -1,19 +1,31 @@
 
-function camelize(str) {
+// function camelize(str) {
 
-    const reduced = str.split("-").reduce((previousValue, currentValue, currentIndex) => {
-        if (currentIndex > 0) {
-            return previousValue + currentValue[0]?.toUpperCase() + currentValue?.slice(1, currentValue?.length);
-        }
-        return previousValue + currentValue
-    });
+//     const reduced = str.split("-").reduce((previousValue, currentValue, currentIndex) => {
+//         if (currentIndex > 0) {
+//             return previousValue + currentValue[0]?.toUpperCase() + currentValue?.slice(1, currentValue?.length);
+//         }
+//         return previousValue + currentValue
+//     });
 
-    return reduced;
+//     return reduced;
+// }
+
+
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+function filterRange(array, low, high) {
+    return array.filter((value) => value >= low && value <= high)
 }
 
 
+let arr = [5, 3, 8, 1];
 
-camelize("background-color") == 'backgroundColor';
-camelize("list-style-image") == 'listStyleImage';
-camelize("-webkit-transition") == 'WebkitTransition';
+let filtered = filterRange(arr, 1, 4);
 
+console.log( filtered ); // 3,1 (matching values)
+
+console.log( arr ); // 5,3,8,1 (not modified)
