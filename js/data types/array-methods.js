884e84391d8a386648d3697607ce9b80fae871ff
bikraman <bikramankarmakar10@gmail.com>
2024-05-02 10:13:27 +0530

    sort users by age

--- js/data types/array-methods.js
+++ js/data types/array-methods.js
@@ -114,3 +114,18 @@
 
 // console.log( usersMapped[0].id ) // 1
 // console.log( usersMapped[0].fullName ) // John Smith
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+const sortByAge = (a) => a.sort((a,b) => a.age - b.age);
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
