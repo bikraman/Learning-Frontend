457b068bb2cf0376d7acf365993e1a96787674a9
bikraman <bikramankarmakar10@gmail.com>
2024-09-30 18:26:09 +0530

    implemented actual binary search

--- ds/binary-search/binary-search.js
+++ ds/binary-search/binary-search.js
@@ -5,21 +5,27 @@
  */
 var search = function(nums, target) {
 
-    const size = nums.length;
+    let start = 0
+    let end = nums.length
+    let pivot = Math.floor(nums.length/2)
 
-    for (let i = 0; i < size/2; i++) {
-        
-        if (nums[i] === target) {
-            return i;
+    while(nums[pivot] !== target) {
+
+        if (end === start + 1)
+            return -1
+
+        if(nums[pivot] > target) {
+            end = pivot;
         }
-        else if (nums[size - i - 1] === target) {
-            return size - i - 1
+        else if (nums[pivot] < target) {
+            start = pivot;
         }
+
+        pivot = Math.floor((start + end)/2)
 
     }
 
-    return -1
-
+    return pivot;
 };
 
-console.log(search([-1,0,3,5,9,12], 9))
+console.log(search([-1,0,3,5,9,12],9))
