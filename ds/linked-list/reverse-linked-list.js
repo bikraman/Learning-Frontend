4cd26db7efcd26cb200858f28d433cc9975419bc
bikraman <bikramankarmakar10@gmail.com>
2024-09-30 14:41:42 +0530

    solved reverse linked list iteratively

--- ds/linked-list/reverse-linked-list.js
+++ ds/linked-list/reverse-linked-list.js
@@ -1,45 +1,41 @@
-
  // Definition for singly-linked list.
-function ListNode(val, next) {
+ function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
 }
-
-list = []
  
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    // list.push(head.val)
 
-    if (head.next === null) {
-        console.log(head.val)
+    if(head === null || head === undefined)
         return head
+
+    list = []
+    let cache = null; 
+
+    printList(head)
+
+    for (let i = 0; i < list.length; i++) {
+        const item = new ListNode(list[i], cache);
+        cache = item;
     }
-    else {
-        reverseList(head.next)  
-    }
-
-    
-
-    console.log(head.val)
-
+    return cache;
 };
 
-// var printList = function(head) {
+var printList = function(head) {
 
-//     console.log(head.val)
+    list.push(head.val)
 
-//     if (head.next === null) {
-//         return -1 
-//     }
-//     else {
-//         printList(head.next)  
-//     }
-
-// };
+    if (head.next === null) {
+        return head;
+    }
+    else {
+        printList(head.next)  
+    }
+}
 
 const fifth = new ListNode(5)
 const fourth = new ListNode(4, fifth)
@@ -48,18 +44,6 @@ const second = new ListNode(2, third)
 const head = new ListNode(1, second)
 
 const reversedList = reverseList(head)
-// console.log(reversedList)
-// console.log(list)
+console.log(reversedList)
 
-// let cache = ListNode(list[list.length - 1], list[list.length - 2])
-
-// for (let i = list.length - 2; i >= 0; i--) {
-//     console.log(list[i])
-
-//     const item = new ListNode(list[i], cache);
-//     cache = item;
-// }
-
-// console.log(cache)
-// printList(head)
 
